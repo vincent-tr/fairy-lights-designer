d2930f7c177fe9e9372370691daffb174f12526f
vincent-tr <vincent.tr@gmail.com>
2025-02-15 13:20:13 +0100

    First rust render

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,16 +1,74 @@
 use wasm_bindgen::prelude::*;
+use js_sys::Uint8ClampedArray;
+use std::slice;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-  
+
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
-pub fn greet() {
+pub fn init() -> Uint8ClampedArray  {
     console_error_panic_hook::set_once();
 
-    alert("Hello, fairy-lights-designer!");
+    console_log!("init");
+    
+    unsafe { 
+        let raw_buffer = slice::from_raw_parts(&raw const BUFFER as *const FrameBuffer as *const u8, std::mem::size_of::<FrameBuffer>());
+        Uint8ClampedArray::view(&raw_buffer)
+    }
+}
+
+const WIDTH: usize = 1000;
+const HEIGHT: usize = 1000;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct Pixel {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+struct FrameBuffer {
+    pixels: [Pixel; WIDTH * HEIGHT],
+}
+
+static mut BUFFER: FrameBuffer = FrameBuffer {
+    pixels: [Pixel {
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 255,
+    }; WIDTH * HEIGHT],
+};
+
+#[wasm_bindgen]
+pub fn render() {
+
+    for i in 0..(WIDTH*HEIGHT) {
+        unsafe {
+            BUFFER.pixels[i] = Pixel {
+                r: 255,
+                g: 0,
+                b: 0,
+                a: 255,
+            };
+        }
+    }
 }
--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,20 @@
 import * as wasm from "../runtime/pkg/runtime";
 
-wasm.greet();
+const WIDTH = 1000;
+const HEIGHT = 1000;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const data = wasm.init();
+const imageData = new ImageData(data, WIDTH, HEIGHT);
+
+requestAnimationFrame(render);
+
+function render() {
+  wasm.render();
+
+  ctx.putImageData(imageData, 0, 0);
+
+  requestAnimationFrame(render);
+}
