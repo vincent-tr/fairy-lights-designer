ad1127fc791ae7f8a5e3869bcdd599972cd86077
vincent-tr <vincent.tr@gmail.com>
2025-02-21 18:14:30 +0100

    format

--- runtime/src/compiler/code_gen.rs
+++ runtime/src/compiler/code_gen.rs
@@ -1,22 +1,19 @@
 use crate::vm::executable::OpCode;
 
-
 pub struct CodeGen {
-  code: Vec<OpCode>,
+    code: Vec<OpCode>,
 }
 
 impl CodeGen {
-  pub fn new() -> Self {
-      CodeGen {
-          code: Vec::new(),
-      }
-  }
+    pub fn new() -> Self {
+        CodeGen { code: Vec::new() }
+    }
 
-  pub fn emit(&mut self, op: OpCode) {
-      self.code.push(op);
-  }
+    pub fn emit(&mut self, op: OpCode) {
+        self.code.push(op);
+    }
 
-  pub fn build(self) -> Vec<OpCode> {
-      self.code
-  }
+    pub fn build(self) -> Vec<OpCode> {
+        self.code
+    }
 }
