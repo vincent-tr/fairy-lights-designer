d14881e034302af0dfbabe573ada415e8f7fa0ca
vincent-tr <vincent.tr@gmail.com>
2025-03-07 12:13:54 +0100

    fix mongodb connection (direction connection)

--- server/src/web_api.rs
+++ server/src/web_api.rs
@@ -1,15 +1,13 @@
 use anyhow::{Context, Result};
 use axum::{
-    extract::{Path, State}, routing::{delete, get, post, put}, Json, Router
+    extract::{Path, State},
+    routing::{delete, get, post, put},
+    Json, Router,
 };
-use mongodb::{
-    bson::{doc, oid::ObjectId, Bson},
-    results::DeleteResult,
-    Client, Collection,
-};
+use futures_util::TryStreamExt;
+use mongodb::{bson::doc, options::ClientOptions, results::DeleteResult, Client, Collection};
 use serde::{Deserialize, Serialize};
 use url::Url;
-use futures_util::TryStreamExt;
 
 use super::WebError;
 
@@ -17,9 +15,9 @@ use super::WebError;
 #[derive(Debug, Deserialize, Serialize)]
 struct Program {
     #[serde(rename = "_id")]
-    id: ObjectId,
+    id: String,
     name: String,
-    content: Bson,
+    content: String,
 }
 
 const COLLECTION_NAME: &str = "programs";
@@ -30,7 +28,9 @@ pub async fn build(url: &str) -> Result<Router> {
         url.path().trim_start_matches('/').to_string()
     };
 
-    let client = Client::with_uri_str(url).await.unwrap();
+    let mut options = ClientOptions::parse(url).await.context("Failed to parse MongoDB URL")?;
+    options.direct_connection = Some(true);
+    let client = Client::with_options(options).context("Failed to connect to MongoDB")?;
     let collection: Collection<Program> = client.database(&db_name).collection(COLLECTION_NAME);
 
     Ok(Router::new()
@@ -48,7 +48,11 @@ async fn create_program(
 ) -> Result<Json<String>, WebError> {
     let result = db.insert_one(input).await?;
 
-    let id= result.inserted_id.as_str().context("Failed to get inserted ID")?.to_string();
+    let id = result
+        .inserted_id
+        .as_str()
+        .context("Failed to get inserted ID")?
+        .to_string();
 
     Ok(Json(id))
 }
@@ -57,7 +61,10 @@ async fn read_program(
     State(db): State<Collection<Program>>,
     Path(id): Path<String>,
 ) -> Result<Json<Program>, WebError> {
-    let program = db.find_one(doc! { "_id": id }).await?.context("Program not found")?;
+    let program = db
+        .find_one(doc! { "_id": id })
+        .await?
+        .context("Program not found")?;
 
     Ok(Json(program))
 }
@@ -67,7 +74,7 @@ async fn update_program(
     Json(input): Json<Program>,
 ) -> Result<Json<()>, WebError> {
     let result = db
-        .replace_one(doc! { "_id": input.id }, input)
+        .replace_one(doc! { "_id": input.id.clone() }, input)
         .await?;
 
     if result.modified_count == 0 {
@@ -81,9 +88,7 @@ async fn delete_program(
     State(db): State<Collection<Program>>,
     Path(id): Path<String>,
 ) -> Result<Json<DeleteResult>, WebError> {
-    let result = db
-        .delete_one(doc! { "_id": id })
-        .await?;
+    let result = db.delete_one(doc! { "_id": id }).await?;
 
     if result.deleted_count == 0 {
         None.context("Program not found")?;
